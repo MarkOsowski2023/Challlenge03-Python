a997671d0c320c33604f96557aecad46f62fcb9a
Mark <markosowski2023@u.northwestern.edu>
2022-07-09 23:06:25 -0500

    making main.py file

---
+++ PyBank/main.py
@@ -0,0 +1,12 @@
+# Importing Modules
+import os
+import csv
+
+# Csv Path
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+# Read Csv
+with open(csvpath) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    print(csv_reader)
+    
