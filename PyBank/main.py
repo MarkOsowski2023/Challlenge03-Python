5197a276ed43b5fd53bf8867dd56b579c99dc9dc
Mark <markosowski2023@u.northwestern.edu>
2022-07-10 16:29:09 -0500

    Fixing my csv path

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,12 @@ import os
 import csv
 
 # Csv Path
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 # Read Csv
 with open(csvpath) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    print(csv_reader)
     
+    for row in csv_reader:
+        print(row)
+        
