ab3cea29e9be442c3a5a54c1d0d9d89a06fa787c
Mark <markosowski2023@u.northwestern.edu>
2022-07-13 22:25:19 -0500

    output file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,9 +3,11 @@ from enum import unique
 import os
 import csv
 
+from nbformat import write
+
 # CSV Path
 csv_path = os.path.join('Resources', 'election_data.csv')
-
+output_file = os.path.join('output', "pypolltxtfile.txt")
 # Define Variables
 canidates = []
 canidates_unique = []
@@ -53,6 +55,15 @@ print("------------------------")
 for i in range(len(set(canidates_unique))):
     print(f"{canidates_unique[i]} : {vote_percent[i]:.0%} ({vote_count[i]})")
 print(f"Winner: {election_winner}")
+
+with open(output_file, 'w') as output:
+    output.write("Election Results\n")
+    output.write("------------------------\n")
+    output.write(f"Total Votes: {votes}\n")
+    output.write("------------------------\n")
+    for i in range(len(set(canidates_unique))):
+        output.write(f"{canidates_unique[i]} : {vote_percent[i]:.0%} ({vote_count[i]})\n")
+    output.write(f"Winner: {election_winner}\n")
 
 
 
